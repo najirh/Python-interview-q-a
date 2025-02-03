05756b93b8e4200e8b9c349b0ae4f95495f62d14
N H (Zero Analyst) <business.najir@gmail.com>
2025-02-03 20:10:31 +0530

    Add files via upload

---
+++ q1.py
@@ -0,0 +1,70 @@
+""" 
+ #Q.1 Can you reverse the below string?
+(without using reversed modules)
+
+s = "Data Analytics"
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# My Solutions1
+# s = "Data Analytics"
+# s = "scitylanA ataD"
+
+# num = len(s)-1
+# reversed_string = ''
+
+
+# while num >= 0:
+#     # print(s[num])
+#     reversed_string += s[num]
+#     # num reducing mechanism
+#     num = num -1
+#     # num -= 1
+
+# print(reversed_string)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+""" 
+Task
+Q.2
+Remove duplicates elements from below list
+(while maintaining the order, also return as list)
+
+my_list = [3, 1, 1, 2, 4, 4]
+
+# Answer i will reveal next video
+"""
+
